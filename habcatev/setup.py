213f49b4bde789dfb3e42e6d1eb0ab1dac613776
Álvaro Peris <alvaroperis_06@hotmail.com>
2021-07-10 15:29:12 +0200

    update

--- habcatev/setup.py
+++ habcatev/setup.py
@@ -6,17 +6,24 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="habcatev",
-    version="0.0.1",
-    author="HabCat team",
+    version="0.0.2",
+    author="HabCat Team",
     author_email="",
     description="Librería para la gestión de eventos",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/alpeza/habcatev",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'paho_mqtt==1.5.1',
+        'PyYAML==5.4.1'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 ) 
+
+
+
