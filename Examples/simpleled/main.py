a7f5921ac12b8f732763acd8aa0063c89d2bae4f
Álvaro Peris <alvaroperis_06@hotmail.com>
2021-07-10 13:35:11 +0200

    update

--- Examples/simpleled/main.py
+++ Examples/simpleled/main.py
@@ -10,11 +10,12 @@ class SimpleExample(habcatev.device.Device.Device):
 
     def on_event(self,topic,data):
         # Lanzamos este codigo cuando se produce un evento
-        print("Topic:" + topic + "  Data: " + data)
+        self.log.logger.info("Ha llegado: Topic:" + topic + "  Data: " + data)
 
     def loop(self):
         # Suponemos que leemos el valor de un sensor y lo escribimos 
         # en un topic
+        self.log.logger.info('Enviando un mensaje a mqtt')
         self.send('mitopico1', random.uniform(10.5, 75.5))
         time.sleep(5)
 
