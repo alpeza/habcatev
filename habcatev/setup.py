8de20616e0f38c2200c5a403902f061e5f7eecc1
Álvaro Peris <alvaroperis_06@hotmail.com>
2021-07-10 16:57:07 +0200

    update

--- habcatev/setup.py
+++ habcatev/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="habcatev",
-    version="0.0.2",
+    version="0.0.3",
     author="HabCat Team",
     author_email="",
     description="Librería para la gestión de eventos",
