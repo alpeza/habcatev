eb5381eaf003ca42cff6013877bb40cfce076403
Álvaro Peris <alvaroperis_06@hotmail.com>
2021-07-10 13:24:28 +0200

    update

--- habcatev/habcatev/common/logs.py
+++ habcatev/habcatev/common/logs.py
@@ -1,13 +1,15 @@
 import os
 import logging
 import logging.config
+currentdir = os.path.dirname(os.path.realpath(__file__))
 
 class Logs(object):
     def __init__(self):
         super(Logs, self).__init__()
+        print(currentdir)
         self.conffile=''
         self._getConfigFile()
-        logging.config.fileConfig('losgging.conf')
+        logging.config.fileConfig('logging.conf')
         self.logger = logging.getLogger('habcatLogger')
 
     def _getConfigFile(self):
